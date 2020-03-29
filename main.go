4c57b7ea3d99672aa4902d10fe9a92efe217bc20
larrygf02 <raulgf_02@hotmail.com>
2020-03-29 16:16:37 -0500

    Go init mux

--- main.go
+++ main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type task struct {
+	ID      int    `json: ID`
+	Name    string `json: Name`
+	Content string `json: Content`
+}
+
+type allTasks []task
+
+var tasks = allTasks{
+	{
+		ID:      1,
+		Name:    "Task one",
+		Content: "Some content",
+	},
+}
+
+func indexRoute(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Bienvenido API en go")
+}
 
 func main() {
-	fmt.Println("Hello world")
+	// Configuración inicial de la ruta
+	router := mux.NewRouter().StrictSlash(true)
+
+	// Definiendo una ruta
+	router.HandleFunc("/", indexRoute)
+
+	// Iniciando un servicdor
+	// logs
+	log.Fatal(http.ListenAndServe(":3000", router))
 }
